b7173b69daecef4f8a9ccb7bad375fd94c965595
AhmettCelik <140803207+AhmettCelik@users.noreply.github.com>
2024-10-04 11:12:37 +0300

    Window class added

--- window.py
+++ window.py
@@ -4,8 +4,21 @@ class Window:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.root = Tk()
-        self.root.title("Maze")
+        self.__root = Tk()
+        self.__root.title("Maze")
         self.canvas = Canvas()
         self.canvas.pack()
+        self.isWindowRunning = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.isWindowRunning = True
+        while self.isWindowRunning:
+            self.redraw()
+
+    def close(self):
         self.isWindowRunning = False

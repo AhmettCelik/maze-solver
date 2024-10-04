3d2d3ee86a2a3add6eb9146eddc2f6a7fa1fabbb
AhmettCelik <140803207+AhmettCelik@users.noreply.github.com>
2024-10-04 11:49:08 +0300

    add Line and Point classes

---
+++ main.py
@@ -0,0 +1,12 @@
+from window import Window
+from line import Line
+from point import Point
+
+def main():
+    win = Window(800, 600)
+    line = Line(Point(50, 50), Point(400, 400))
+    win.draw_line(line, "black")
+    win.wait_for_close()
+
+if __name__ == "__main__":
+    main()

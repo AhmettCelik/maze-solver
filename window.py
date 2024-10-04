dedb78c5a7ca46a3587cdf3ff63c6e3a5ba3aa16
AhmettCelik <140803207+AhmettCelik@users.noreply.github.com>
2024-10-04 11:03:44 +0300

    Window constructor

--- window.py
+++ window.py
@@ -1,1 +1,11 @@
 from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.root = Tk()
+        self.root.title("Maze")
+        self.canvas = Canvas()
+        self.canvas.pack()
+        self.isWindowRunning = False

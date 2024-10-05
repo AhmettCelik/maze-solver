cf102666fa510c7a510c39691076e4ad02a3ae77
AhmettCelik <140803207+AhmettCelik@users.noreply.github.com>
2024-10-05 16:58:38 +0300

    maze class

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ from window import Window
 from line import Line
 from point import Point
 from cell import Cell
+from maze import Maze
 
 def main():
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
     win = Window(800, 600)
 
-    c1 = Cell(win)
-    c1.has_right_wall = False
-    c1.draw(300, 400, 400, 500)
-
-    c2 = Cell(win)
-    c2.has_left_wall = False
-    c2.draw(600, 400, 700, 500)
-
-    c1.draw_move(c2, True)
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
 
     win.wait_for_close()
 
